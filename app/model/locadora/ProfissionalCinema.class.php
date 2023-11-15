a98018bedd28e99f1310601255e5202fd7ed3f61
Breno Nascimento <brenonsmacedo@gmail.com>
2023-11-15 17:43:55 -0300

    sy

---
+++ app/model/locadora/ProfissionalCinema.class.php
@@ -0,0 +1,18 @@
+<?php 
+class ProfissionalCinema_class extends TRecord
+{
+    const TABLENAME = 'Profissional_Cinema';
+    const PRIMARYKEY= 'id';
+    const IDPOLICY =  'max'; // {max, serial}
+    
+    public function __construct($id = NULL)
+    {
+        parent::__construct($id);
+        parent::addAttribute('nome_prof');
+        parent::addAttribute('sobrenome_prof');
+        parent::addAttribute('pais_origem');
+        parent::addAttribute('tipo');
+    }
+}
+
+?>
